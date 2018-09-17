hquip/frida-core

commit 73d48bb028c37ec62810d2634d99a85f55466a74

Add support for eternalizing scripts
Such scripts stay loaded indefinitely, and the agent stays resident
for the remainder of the process lifetime.

lib/agent/agent.vala
@@ -28,7 +28,14 @@ namespace Frida.Agent {
 			set;
 		}
 
+		public bool has_eternalized_scripts {
+			get {
+				return !eternalized_scripts.is_empty;
+			}
+		}
+
 		private void * agent_pthread;
+		private Thread<bool> agent_gthread;
 
 		private MainContext main_context;
 		private MainLoop main_loop;
@@ -40,6 +47,7 @@ namespace Frida.Agent {
 		private uint pending_calls = 0;
 		private Gee.Promise<bool> pending_close;
 		private Gee.HashSet<AgentClient> clients = new Gee.HashSet<AgentClient> ();
+		private Gee.ArrayList<Gum.Script> eternalized_scripts = new Gee.ArrayList<Gum.Script> ();
 
 		private Gum.ScriptBackend script_backend;
 		private ExitMonitor exit_monitor;
@@ -115,6 +123,10 @@ namespace Frida.Agent {
 #endif
 					unload_policy = DEFERRED;
 					return;
+				} else if (shared_instance.has_eternalized_scripts) {
+					unload_policy = RESIDENT;
+					shared_instance.keep_running_eternalized_scripts ();
+					return;
 				} else {
 					release_shared_instance ();
 				}
@@ -144,6 +156,10 @@ namespace Frida.Agent {
 						Gum.Cloak.remove_file_descriptor (injector_state.fifo_fd);
 #endif
 					unload_policy = DEFERRED;
+					return;
+				} else if (shared_instance.has_eternalized_scripts) {
+					unload_policy = RESIDENT;
+					shared_instance.keep_running_eternalized_scripts ();
 					return;
 				} else {
 					release_shared_instance ();
@@ -211,6 +227,20 @@ namespace Frida.Agent {
 			main_context.pop_thread_default ();
 		}
 
+		private void keep_running_eternalized_scripts () {
+			agent_gthread = new Thread<bool> ("frida-eternal-agent", () => {
+				var ignore_scope = new ThreadIgnoreScope ();
+
+				main_context.push_thread_default ();
+				main_loop.run ();
+				main_context.pop_thread_default ();
+
+				ignore_scope = null;
+
+				return true;
+			});
+		}
+
 		private async void prepare_to_exit () {
 			yield prepare_for_termination ();
 		}
@@ -233,7 +263,13 @@ namespace Frida.Agent {
 				do_prepare_to_fork.begin ();
 				return false;
 			});
-			Environment._join_pthread (agent_pthread);
+			if (agent_gthread != null) {
+				agent_gthread.join ();
+				agent_gthread = null;
+			} else {
+				Environment._join_pthread (agent_pthread);
+			}
+			agent_pthread = null;
 
 			GumJS.prepare_to_fork ();
 			Gum.prepare_to_fork ();
@@ -244,15 +280,17 @@ namespace Frida.Agent {
 		private async void do_prepare_to_fork () {
 			stop_reason = FORK;
 
-			try {
-				fork_parent_pid = Posix.getpid ();
-				fork_child_id = yield controller.prepare_to_fork (fork_parent_pid, out fork_parent_injectee_id, out fork_child_injectee_id, out fork_child_socket);
-			} catch (GLib.Error e) {
+			if (controller != null) {
+				try {
+					fork_parent_pid = Posix.getpid ();
+					fork_child_id = yield controller.prepare_to_fork (fork_parent_pid, out fork_parent_injectee_id, out fork_child_injectee_id, out fork_child_socket);
+				} catch (GLib.Error e) {
 #if ANDROID
-				error ("Oops, SELinux rule probably missing for your system. Symptom: %s", e.message);
+					error ("Oops, SELinux rule probably missing for your system. Symptom: %s", e.message);
 #else
-				error ("%s", e.message);
+					error ("%s", e.message);
 #endif
+				}
 			}
 
 			main_loop.quit ();
@@ -310,33 +348,43 @@ namespace Frida.Agent {
 		}
 
 		private async void recreate_agent_thread (ForkActor actor) {
-			uint pid, injectee_id;
-			if (actor == PARENT) {
-				pid = fork_parent_pid;
-				injectee_id = fork_parent_injectee_id;
-			} else if (actor == CHILD) {
-				yield close_all_clients ();
+			if (controller != null) {
+				uint pid, injectee_id;
+				if (actor == PARENT) {
+					pid = fork_parent_pid;
+					injectee_id = fork_parent_injectee_id;
+				} else if (actor == CHILD) {
+					yield close_all_clients ();
 
-				var stream = SocketConnection.factory_create_connection (fork_child_socket);
-				yield setup_connection_with_stream (stream);
+					var stream = SocketConnection.factory_create_connection (fork_child_socket);
+					yield setup_connection_with_stream (stream);
 
-				pid = fork_child_pid;
-				injectee_id = fork_child_injectee_id;
+					pid = fork_child_pid;
+					injectee_id = fork_child_injectee_id;
+				} else {
+					assert_not_reached ();
+				}
+
+				try {
+					yield controller.recreate_agent_thread (pid, injectee_id);
+				} catch (GLib.Error e) {
+					assert_not_reached ();
+				}
 			} else {
-				assert_not_reached ();
-			}
+				agent_gthread = new Thread<bool> ("frida-eternal-agent", () => {
+					var ignore_scope = new ThreadIgnoreScope ();
+					run_after_fork ();
+					ignore_scope = null;
 
-			try {
-				yield controller.recreate_agent_thread (pid, injectee_id);
-			} catch (GLib.Error e) {
-				assert_not_reached ();
+					return true;
+				});
 			}
 
 			main_loop.quit ();
 		}
 
 		private async void finish_recovery_from_fork (ForkActor actor, string? identifier) {
-			if (actor == CHILD) {
+			if (actor == CHILD && controller != null) {
 				var info = HostChildInfo (fork_child_pid, fork_parent_pid, ChildOrigin.FORK);
 				if (identifier != null)
 					info.identifier = identifier;
@@ -413,6 +461,7 @@ namespace Frida.Agent {
 			var client = new AgentClient (this, id);
 			clients.add (client);
 			client.closed.connect (on_client_closed);
+			client.script_eternalized.connect (on_script_eternalized);
 
 			try {
 				AgentSession session = client;
@@ -444,8 +493,13 @@ namespace Frida.Agent {
 				client.registration_id = 0;
 			}
 
+			client.script_eternalized.disconnect (on_script_eternalized);
 			client.closed.disconnect (on_client_closed);
 			clients.remove (client);
+		}
+
+		private void on_script_eternalized (Gum.Script script) {
+			eternalized_scripts.add (script);
 		}
 
 		private async void unload () throws Error {
@@ -723,6 +777,7 @@ namespace Frida.Agent {
 
 	private class AgentClient : Object, AgentSession {
 		public signal void closed (AgentClient client);
+		public signal void script_eternalized (Gum.Script script);
 
 		public weak Runner runner {
 			get;
@@ -824,6 +879,12 @@ namespace Frida.Agent {
 		public async void load_script (AgentScriptId sid) throws Error {
 			var engine = get_script_engine ();
 			yield engine.load_script (sid);
+		}
+
+		public async void eternalize_script (AgentScriptId sid) throws Error {
+			var engine = get_script_engine ();
+			var script = engine.eternalize_script (sid);
+			script_eternalized (script);
 		}
 
 		public async void post_to_script (AgentScriptId sid, string message, bool has_data, uint8[] data) throws Error {

lib/agent/script-engine.vala
@@ -101,6 +101,15 @@ namespace Frida.Agent {
 			yield instance.load ();
 		}
 
+		public Gum.Script eternalize_script (AgentScriptId sid) throws Error {
+			var instance = instances[sid.handle];
+			if (instance == null)
+				throw new Error.INVALID_ARGUMENT ("Invalid script ID");
+			var script = instance.eternalize ();
+			instances.unset (sid.handle);
+			return script;
+		}
+
 		public void post_to_script (AgentScriptId sid, string message, Bytes? data = null) throws Error {
 			var instance = instances[sid.handle];
 			if (instance == null)
@@ -157,6 +166,7 @@ namespace Frida.Agent {
 			private enum State {
 				CREATED,
 				LOADED,
+				ETERNALIZED,
 				DISPOSED,
 				UNLOADED,
 				DESTROYED
@@ -226,6 +236,17 @@ namespace Frida.Agent {
 				state = DESTROYED;
 
 				destroy_request.set_value (true);
+			}
+
+			public Gum.Script eternalize () throws Error {
+				if (state != LOADED)
+					throw new Error.INVALID_OPERATION ("Only loaded scripts may be eternalized");
+
+				state = ETERNALIZED;
+
+				var result = script;
+				script = null;
+				return result;
 			}
 
 			public async void ensure_dispose_called () {

lib/gadget/gadget.vala
@@ -1205,6 +1205,7 @@ namespace Frida.Gadget {
 		private unowned Gum.ScriptBackend script_backend = null;
 		private DBusServer server;
 		private Gee.HashMap<DBusConnection, Client> clients = new Gee.HashMap<DBusConnection, Client> ();
+		private Gee.ArrayList<Gum.Script> eternalized_scripts = new Gee.ArrayList<Gum.Script> ();
 
 		public Server (Config config, Location location) throws Error {
 			Object (
@@ -1225,9 +1226,7 @@ namespace Frida.Gadget {
 				if (server == null)
 					return false;
 
-				clients[connection] = new Client (this, connection);
-				connection.on_closed.connect (on_connection_closed);
-
+				attach_client (connection);
 				return true;
 			});
 
@@ -1261,6 +1260,8 @@ namespace Frida.Gadget {
 				Client client;
 				clients.unset (connection, out client);
 				yield client.shutdown ();
+
+				detach_client (client);
 			}
 		}
 
@@ -1281,13 +1282,33 @@ namespace Frida.Gadget {
 			config.runtime = RuntimeFlavor.JIT;
 		}
 
+		private void attach_client (DBusConnection connection) {
+			var client = new Client (this, connection);
+			clients[connection] = client;
+			connection.on_closed.connect (on_connection_closed);
+
+			client.script_eternalized.connect (on_script_eternalized);
+		}
+
+		private void detach_client (Client client) {
+			client.script_eternalized.disconnect (on_script_eternalized);
+		}
+
 		private void on_connection_closed (DBusConnection connection, bool remote_peer_vanished, GLib.Error? error) {
 			Client client;
-			if (clients.unset (connection, out client))
+			if (clients.unset (connection, out client)) {
 				client.shutdown.begin ();
+				detach_client (client);
+			}
+		}
+
+		private void on_script_eternalized (Gum.Script script) {
+			eternalized_scripts.add (script);
 		}
 
 		private class Client : Object, HostSession {
+			public signal void script_eternalized (Gum.Script script);
+
 			private unowned Server server;
 			private DBusConnection connection;
 			private uint host_registration_id;
@@ -1400,6 +1421,7 @@ namespace Frida.Gadget {
 				var session = new ClientSession (server, id);
 				sessions.add (session);
 				session.closed.connect (on_session_closed);
+				session.script_eternalized.connect (on_script_eternalized);
 
 				try {
 					AgentSession s = session;
@@ -1422,10 +1444,15 @@ namespace Frida.Gadget {
 			private void on_session_closed (ClientSession session) {
 				connection.unregister_object (session.registration_id);
 
+				session.script_eternalized.disconnect (on_script_eternalized);
 				session.closed.disconnect (on_session_closed);
 				sessions.remove (session);
 
 				agent_session_destroyed (session.id, SessionDetachReason.APPLICATION_REQUESTED);
+			}
+
+			private void on_script_eternalized (Gum.Script script) {
+				script_eternalized (script);
 			}
 
 			private void validate_pid (uint pid) throws Error {
@@ -1436,6 +1463,7 @@ namespace Frida.Gadget {
 
 		private class ClientSession : Object, AgentSession {
 			public signal void closed (ClientSession session);
+			public signal void script_eternalized (Gum.Script script);
 
 			public weak Server server {
 				get;
@@ -1520,6 +1548,12 @@ namespace Frida.Gadget {
 			public async void load_script (AgentScriptId sid) throws Error {
 				var engine = get_script_engine ();
 				yield engine.load_script (sid);
+			}
+
+			public async void eternalize_script (AgentScriptId sid) throws Error {
+				var engine = get_script_engine ();
+				var script = engine.eternalize_script (sid);
+				script_eternalized (script);
 			}
 
 			public async void post_to_script (AgentScriptId sid, string message, bool has_data, uint8[] data) throws Error {

lib/gadget/script-engine.vala
@@ -101,6 +101,15 @@ namespace Frida.Gadget {
 			yield instance.load ();
 		}
 
+		public Gum.Script eternalize_script (AgentScriptId sid) throws Error {
+			var instance = instances[sid.handle];
+			if (instance == null)
+				throw new Error.INVALID_ARGUMENT ("Invalid script ID");
+			var script = instance.eternalize ();
+			instances.unset (sid.handle);
+			return script;
+		}
+
 		public void post_to_script (AgentScriptId sid, string message, Bytes? data = null) throws Error {
 			var instance = instances[sid.handle];
 			if (instance == null)
@@ -157,6 +166,7 @@ namespace Frida.Gadget {
 			private enum State {
 				CREATED,
 				LOADED,
+				ETERNALIZED,
 				DISPOSED,
 				UNLOADED,
 				DESTROYED
@@ -226,6 +236,17 @@ namespace Frida.Gadget {
 				state = DESTROYED;
 
 				destroy_request.set_value (true);
+			}
+
+			public Gum.Script eternalize () throws Error {
+				if (state != LOADED)
+					throw new Error.INVALID_OPERATION ("Only loaded scripts may be eternalized");
+
+				state = ETERNALIZED;
+
+				var result = script;
+				script = null;
+				return result;
 			}
 
 			public async void ensure_dispose_called () {

lib/interfaces/session.vala
@@ -48,6 +48,7 @@ namespace Frida {
 		public abstract async uint8[] compile_script (string name, string source) throws GLib.Error;
 		public abstract async void destroy_script (AgentScriptId sid) throws GLib.Error;
 		public abstract async void load_script (AgentScriptId sid) throws GLib.Error;
+		public abstract async void eternalize_script (AgentScriptId sid) throws GLib.Error;
 		public abstract async void post_to_script (AgentScriptId sid, string message, bool has_data, uint8[] data) throws GLib.Error;
 		public signal void message_from_script (AgentScriptId sid, string message, bool has_data, uint8[] data);
 

src/frida.vala
@@ -1940,6 +1940,27 @@ namespace Frida {
 			}
 		}
 
+		public async void eternalize () throws Error {
+			check_open ();
+
+			try {
+				yield session.session.eternalize_script (AgentScriptId (id));
+				yield _do_close (false);
+			} catch (GLib.Error e) {
+				throw Marshal.from_dbus (e);
+			}
+		}
+
+		public void eternalize_sync () throws Error {
+			(create<EternalizeTask> () as EternalizeTask).start_and_wait_for_completion ();
+		}
+
+		private class EternalizeTask : ScriptTask<void> {
+			protected override async void perform_operation () throws Error {
+				yield parent.eternalize ();
+			}
+		}
+
 		public async void post (string message, Bytes? data = null) throws Error {
 			check_open ();
 

commit 920dffcfe105c3e7333feda55a5432fdb97098d8

Fix child-gating regression
Broken by 73d48bb028c37ec62810d2634d99a85f55466a74.

lib/agent/agent.vala
@@ -348,23 +348,25 @@ namespace Frida.Agent {
 		}
 
 		private async void recreate_agent_thread (ForkActor actor) {
-			if (controller != null) {
-				uint pid, injectee_id;
-				if (actor == PARENT) {
-					pid = fork_parent_pid;
-					injectee_id = fork_parent_injectee_id;
-				} else if (actor == CHILD) {
-					yield close_all_clients ();
+			uint pid, injectee_id;
+			if (actor == PARENT) {
+				pid = fork_parent_pid;
+				injectee_id = fork_parent_injectee_id;
+			} else if (actor == CHILD) {
+				yield close_all_clients ();
 
+				if (fork_child_socket != null) {
 					var stream = SocketConnection.factory_create_connection (fork_child_socket);
 					yield setup_connection_with_stream (stream);
-
-					pid = fork_child_pid;
-					injectee_id = fork_child_injectee_id;
-				} else {
-					assert_not_reached ();
 				}
 
+				pid = fork_child_pid;
+				injectee_id = fork_child_injectee_id;
+			} else {
+				assert_not_reached ();
+			}
+
+			if (controller != null) {
 				try {
 					yield controller.recreate_agent_thread (pid, injectee_id);
 				} catch (GLib.Error e) {