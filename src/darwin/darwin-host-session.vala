hquip/frida-core

commit 8e3cd556c58839997892ff6d478cac7ea39df6e6

Bump iOS memory limits for non-apps before attaching
To be able to instrument daemons with low memory limits.

We revert back to the previous limit when the last injection payload
gets unloaded.

src/darwin/frida-helper-backend-glue.m
@@ -170,6 +170,7 @@ struct _FridaInjectInstance
 {
   guint id;
 
+  guint pid;
   mach_port_t task;
 
   mach_vm_address_t payload_address;
@@ -309,7 +310,7 @@ static guint32 frida_spawn_instance_overwrite_arm64_instruction (FridaSpawnInsta
 
 static void frida_make_pipe (int fds[2]);
 
-static FridaInjectInstance * frida_inject_instance_new (FridaDarwinHelperBackend * backend, guint id);
+static FridaInjectInstance * frida_inject_instance_new (FridaDarwinHelperBackend * backend, guint id, guint pid);
 static FridaInjectInstance * frida_inject_instance_clone (const FridaInjectInstance * instance, guint id);
 static void frida_inject_instance_free (FridaInjectInstance * instance);
 static gboolean frida_inject_instance_task_did_not_exec (FridaInjectInstance * instance);
@@ -1309,6 +1310,28 @@ frida_configure_terminal_attributes (gint fd)
   tcsetattr (fd, 0, &tios);
 }
 
+gboolean
+frida_darwin_helper_backend_is_application_process (guint pid)
+{
+  gboolean result = FALSE;
+  NSAutoreleasePool * pool;
+  NSString * identifier;
+
+  pool = [[NSAutoreleasePool alloc] init];
+
+  identifier = _frida_get_springboard_api ()->SBSCopyDisplayIdentifierForProcessID (pid);
+  if (identifier != nil)
+  {
+    result = TRUE;
+
+    [identifier release];
+  }
+
+  [pool release];
+
+  return result;
+}
+
 #else
 
 void
@@ -1334,6 +1357,12 @@ _frida_darwin_helper_backend_kill_process (guint pid)
 void
 _frida_darwin_helper_backend_kill_application (const gchar * identifier)
 {
+}
+
+gboolean
+frida_darwin_helper_backend_is_application_process (guint pid)
+{
+  return FALSE;
 }
 
 #endif
@@ -1772,7 +1801,7 @@ _frida_darwin_helper_backend_inject_into_task (FridaDarwinHelperBackend * self, 
 
   self_task = mach_task_self ();
 
-  instance = frida_inject_instance_new (self, self->next_id++);
+  instance = frida_inject_instance_new (self, self->next_id++, pid);
   mach_port_mod_refs (self_task, task, MACH_PORT_RIGHT_SEND, 1);
   instance->task = task;
 
@@ -1980,6 +2009,8 @@ _frida_darwin_helper_backend_inject_into_task (FridaDarwinHelperBackend * self, 
     goto failure;
 
   gee_abstract_map_set (GEE_ABSTRACT_MAP (self->inject_instances), GUINT_TO_POINTER (instance->id), instance);
+
+  _frida_darwin_helper_backend_on_inject_instance_loaded (self, instance->pid);
 
   result = instance->id;
   goto beach;
@@ -3102,13 +3133,14 @@ frida_make_pipe (int fds[2])
 }
 
 static FridaInjectInstance *
-frida_inject_instance_new (FridaDarwinHelperBackend * backend, guint id)
+frida_inject_instance_new (FridaDarwinHelperBackend * backend, guint id, guint pid)
 {
   FridaInjectInstance * instance;
 
   instance = g_slice_new (FridaInjectInstance);
   instance->id = id;
 
+  instance->pid = pid;
   instance->task = MACH_PORT_NULL;
 
   instance->payload_address = 0;
@@ -3174,6 +3206,11 @@ frida_inject_instance_free (FridaInjectInstance * instance)
       frida_inject_instance_task_did_not_exec (instance))
   {
     mach_vm_deallocate (instance->task, instance->payload_address, instance->payload_size);
+    _frida_darwin_helper_backend_on_inject_instance_unloaded (instance->backend, instance->pid);
+  }
+  else
+  {
+    _frida_darwin_helper_backend_on_inject_instance_detached (instance->backend, instance->pid);
   }
 
   if (agent_context != NULL)

src/darwin/frida-helper-backend.vala
@@ -51,8 +51,10 @@ namespace Frida {
 			if (ElectraPolicySoftener.is_available ())
 				policy_softener = new ElectraPolicySoftener ();
 			else
+				policy_softener = new IOSPolicySoftener ();
+#else
+			policy_softener = new NullPolicySoftener ();
 #endif
-				policy_softener = new NullPolicySoftener ();
 		}
 
 		~DarwinHelperBackend () {
@@ -519,6 +521,18 @@ namespace Frida {
 			Source.remove (timer);
 		}
 
+		public void _on_inject_instance_loaded (uint pid) {
+			policy_softener.retain (pid);
+		}
+
+		public void _on_inject_instance_unloaded (uint pid) {
+			policy_softener.release (pid);
+		}
+
+		public void _on_inject_instance_detached (uint pid) {
+			policy_softener.forget (pid);
+		}
+
 		public uint task_for_pid (uint pid) throws Error {
 			if (kernel_agent != null)
 				return kernel_agent.task_for_pid (pid);
@@ -552,6 +566,7 @@ namespace Frida {
 		protected extern void _resume_process_fast (uint task) throws Error;
 		protected extern static void _kill_process (uint pid);
 		protected extern static void _kill_application (string identifier);
+		public extern static bool is_application_process (uint pid);
 		protected extern void * _create_spawn_instance (uint pid);
 		protected extern void _prepare_spawn_instance_for_injection (void * instance, uint task) throws Error;
 		protected extern void _resume_spawn_instance (void * instance);

src/darwin/policy-softener.vala
@@ -1,15 +1,149 @@
 namespace Frida {
 	public interface PolicySoftener : Object {
-		public abstract void soften (uint pid) throws Error;
+		public abstract void soften (uint pid);
+		public abstract void retain (uint pid);
+		public abstract void release (uint pid);
+		public abstract void forget (uint pid);
 	}
 
 	public class NullPolicySoftener : Object, PolicySoftener {
-		public void soften (uint pid) throws Error {
+		public void soften (uint pid) {
+		}
+
+		public void retain (uint pid) {
+		}
+
+		public void release (uint pid) {
+		}
+
+		public void forget (uint pid) {
 		}
 	}
 
 #if IOS
-	public class ElectraPolicySoftener : Object, PolicySoftener {
+	public class IOSPolicySoftener : Object, PolicySoftener {
+		private Gee.HashMap<uint, ProcessEntry> process_entries = new Gee.HashMap<uint, ProcessEntry> ();
+
+		public void soften (uint pid) {
+			if (process_entries.has_key (pid))
+				return;
+
+			var entry = perform_softening (pid);
+
+			var expiry_source = new TimeoutSource.seconds (20);
+			expiry_source.set_callback (() => {
+				entry.expiry_source = null;
+
+				forget (pid);
+
+				return false;
+			});
+			expiry_source.attach (MainContext.get_thread_default ());
+			entry.expiry_source = expiry_source;
+		}
+
+		public void retain (uint pid) {
+			var entry = process_entries[pid];
+			if (entry == null)
+				entry = perform_softening (pid);
+			entry.cancel_expiry ();
+			entry.usage_count++;
+		}
+
+		public void release (uint pid) {
+			var entry = process_entries[pid];
+			if (entry == null)
+				return;
+			assert (entry.usage_count != 0);
+			entry.usage_count--;
+			if (entry.usage_count == 0) {
+				revert_softening (entry);
+				process_entries.unset (pid);
+			}
+		}
+
+		public void forget (uint pid) {
+			ProcessEntry entry;
+			if (process_entries.unset (pid, out entry))
+				entry.cancel_expiry ();
+		}
+
+		protected virtual ProcessEntry perform_softening (uint pid) {
+			MemlimitProperties? saved_memory_limits = null;
+
+			var props = MemlimitProperties ();
+			int status = memorystatus_control (GET_MEMLIMIT_PROPERTIES, (int32) pid, 0, &props, sizeof (MemlimitProperties));
+			if (status == 0) {
+				bool is_app_process = DarwinHelperBackend.is_application_process (pid);
+				if (!is_app_process) {
+					saved_memory_limits = props;
+
+					props.active = int32.MAX;
+					props.active_attr = 0;
+					props.inactive = int32.MAX;
+					props.inactive_attr = 0;
+					status = memorystatus_control (SET_MEMLIMIT_PROPERTIES, (int32) pid, 0, &props, sizeof (MemlimitProperties));
+				}
+			}
+
+			var entry = new ProcessEntry (pid, saved_memory_limits);
+			process_entries[pid] = entry;
+
+			return entry;
+		}
+
+		protected virtual void revert_softening (ProcessEntry entry) {
+			if (entry.saved_memory_limits != null) {
+				memorystatus_control (SET_MEMLIMIT_PROPERTIES, (int32) entry.pid, 0, (void *) entry.saved_memory_limits, sizeof (MemlimitProperties));
+			}
+		}
+
+		protected class ProcessEntry {
+			public uint pid;
+			public uint usage_count;
+			public MemlimitProperties? saved_memory_limits;
+			public Source? expiry_source;
+
+			public ProcessEntry (uint pid, MemlimitProperties? saved_memory_limits) {
+				this.pid = pid;
+				this.usage_count = 0;
+				this.saved_memory_limits = saved_memory_limits;
+			}
+
+			~ProcessEntry () {
+				cancel_expiry ();
+			}
+
+			public void cancel_expiry () {
+				if (expiry_source != null) {
+					expiry_source.destroy ();
+					expiry_source = null;
+				}
+			}
+		}
+
+		[CCode (cname = "memorystatus_control")]
+		private extern static int memorystatus_control (MemoryStatusCommand command, int32 pid, uint32 flags, void * buffer, size_t buffer_size);
+
+		private enum MemoryStatusCommand {
+			SET_MEMLIMIT_PROPERTIES = 7,
+			GET_MEMLIMIT_PROPERTIES = 8,
+		}
+
+		protected struct MemlimitProperties {
+			public int32 active;
+			public MemlimitAttributes active_attr;
+			public int32 inactive;
+			public MemlimitAttributes inactive_attr;
+		}
+
+		[Flags]
+		protected enum MemlimitAttributes {
+			FATAL = 1,
+		}
+	}
+
+	public class ElectraPolicySoftener : IOSPolicySoftener {
 		private const string LIBJAILBREAK_PATH = "/usr/lib/libjailbreak.dylib";
 
 		private Module libjailbreak;
@@ -40,8 +174,10 @@ namespace Frida {
 			return FileUtils.test (LIBJAILBREAK_PATH, FileTest.EXISTS);
 		}
 
-		public void soften (uint pid) throws Error {
+		protected override IOSPolicySoftener.ProcessEntry perform_softening (uint pid) {
 			entitle_and_platformize (pid);
+
+			return base.perform_softening (pid);
 		}
 
 		private void entitle_and_platformize (uint pid) {

commit 4ad95591f4b63b3b3281dbb934f4e31be0c4de8c

Fix iOS early instrumentation stability regression
The dynamic memory limit adjustments introduced in 8e3cd55 made the
policy softening stateful, keeping track of whether a given PID that
we've got an agent in has already been policy-softened. That means a
cache invalidation bug may break exec-transitions because we fail to
entitle/platformize the post-exec version of the process. I.e. when
xpcproxy becomes the app or service being spawned.

Turns out we did not take into account the case where the invalidation
happened after we considered the exec-transition complete. This meant
that the spawned/spawn-gated process would die pretty quick, and make
attach() fail with the following error:

    Expecting to read a single byte for receiving credentials but read
    zero bytes

We fix this issue by explicitly notifying the backend when an exec has
completed. The backend is then able to flush its dispatch queue, and
eagerly clean up any injection instances for the given PID.

src/darwin/darwin-host-session.vala
@@ -228,6 +228,7 @@ namespace Frida {
 
 		protected override async void await_exec_transition (uint pid) throws Error {
 			yield helper.wait_until_suspended (pid);
+			yield helper.notify_exec_completed (pid);
 		}
 
 		protected override async void cancel_exec_transition (uint pid) throws Error {
@@ -560,6 +561,7 @@ namespace Frida {
 			yield wait_for_unload ();
 
 			yield helper.wait_until_suspended (pid);
+			yield helper.notify_exec_completed (pid);
 		}
 
 		protected override async uint get_target_pid () throws Error {

src/darwin/frida-helper-backend-glue.m
@@ -582,6 +582,17 @@ _frida_darwin_helper_backend_destroy_context (FridaDarwinHelperBackend * self)
   g_slice_free (FridaHelperContext, ctx);
 }
 
+void
+_frida_darwin_helper_backend_schedule_on_dispatch_queue (FridaDarwinHelperBackend * self, FridaDarwinHelperBackendDispatchWorker worker, gpointer user_data)
+{
+  FridaHelperContext * ctx = self->context;
+
+  dispatch_async (ctx->dispatch_queue, ^
+  {
+    worker (user_data);
+  });
+}
+
 guint
 _frida_darwin_helper_backend_spawn (FridaDarwinHelperBackend * self, const gchar * path,
     FridaHostSpawnOptions * options, FridaStdioPipes ** pipes, GError ** error)
@@ -2208,6 +2219,12 @@ frida_inject_instance_on_posix_thread_dead (void * context)
   FridaInjectInstance * self = context;
 
   _frida_darwin_helper_backend_on_posix_thread_dead (self->backend, self->id);
+}
+
+guint
+_frida_darwin_helper_backend_get_pid_of_inject_instance (FridaDarwinHelperBackend * self, void * instance)
+{
+  return ((FridaInjectInstance *) instance)->pid;
 }
 
 void

src/darwin/frida-helper-backend.vala
@@ -16,6 +16,7 @@ namespace Frida {
 			}
 		}
 
+		protected delegate void DispatchWorker ();
 		protected delegate void LaunchCompletionHandler (owned StdioPipes? pipes, owned Error? error);
 
 		public void * context;
@@ -171,6 +172,26 @@ namespace Frida {
 				process_next_output_from.begin (new UnixInputStream (pipes.output, false), pid, 1, pipes);
 				process_next_output_from.begin (new UnixInputStream (pipes.error, false), pid, 2, pipes);
 			}
+		}
+
+		public async void notify_exec_completed (uint pid) throws Error {
+			yield flush_dispatch_queue ();
+
+			var dead_instances = new Gee.ArrayList<void *> ();
+			foreach (var instance in inject_cleaner_by_instance.keys) {
+				if (_get_pid_of_inject_instance (instance) == pid)
+					dead_instances.add (instance);
+			}
+
+			foreach (var instance in dead_instances) {
+				uint source_id;
+				inject_cleaner_by_instance.unset (instance, out source_id);
+				Source.remove (source_id);
+
+				_free_inject_instance (instance);
+			}
+
+			policy_softener.forget (pid);
 		}
 
 		private void on_launch_completed (PendingLaunch pending) {
@@ -548,6 +569,16 @@ namespace Frida {
 			spawn_removed (info);
 		}
 
+		private async void flush_dispatch_queue () {
+			_schedule_on_dispatch_queue (() => {
+				Idle.add (() => {
+					flush_dispatch_queue.callback ();
+					return false;
+				});
+			});
+			yield;
+		}
+
 		public extern static PipeEndpoints make_pipe_endpoints (uint local_task, uint remote_pid, uint remote_task) throws Error;
 
 		public extern static uint task_for_pid_fallback (uint pid) throws Error;
@@ -558,6 +589,7 @@ namespace Frida {
 
 		protected extern void _create_context ();
 		protected extern void _destroy_context ();
+		protected extern void _schedule_on_dispatch_queue (DispatchWorker worker);
 
 		protected extern uint _spawn (string path, HostSpawnOptions options, out StdioPipes? pipes) throws Error;
 		protected extern static void _launch (string identifier, HostSpawnOptions options, LaunchCompletionHandler on_complete);
@@ -576,6 +608,7 @@ namespace Frida {
 		protected extern uint _demonitor_and_clone_injectee_state (void * instance);
 		protected extern void _recreate_injectee_thread (void * instance, uint pid, uint task) throws Error;
 		protected extern void _join_inject_instance_posix_thread (void * instance, void * posix_thread);
+		protected extern uint _get_pid_of_inject_instance (void * instance);
 		protected extern void _free_inject_instance (void * instance);
 	}
 

src/darwin/frida-helper-process.vala
@@ -121,6 +121,15 @@ namespace Frida {
 			}
 		}
 
+		public async void notify_exec_completed (uint pid) throws Error {
+			var helper = yield obtain ();
+			try {
+				yield helper.notify_exec_completed (pid);
+			} catch (GLib.Error e) {
+				throw Marshal.from_dbus (e);
+			}
+		}
+
 		public async void wait_until_suspended (uint pid) throws Error {
 			var helper = yield obtain ();
 			try {

src/darwin/frida-helper-service.vala
@@ -162,6 +162,10 @@ namespace Frida {
 			yield backend.notify_launch_completed (identifier, pid);
 		}
 
+		public async void notify_exec_completed (uint pid) throws Error {
+			yield backend.notify_exec_completed (pid);
+		}
+
 		public async void wait_until_suspended (uint pid) throws Error {
 			yield backend.wait_until_suspended (pid);
 		}

src/darwin/frida-helper-types.vala
@@ -19,6 +19,7 @@ namespace Frida {
 		public abstract async uint spawn (string path, HostSpawnOptions options) throws Error;
 		public abstract async void launch (string identifier, HostSpawnOptions options) throws Error;
 		public abstract async void notify_launch_completed (string identifier, uint pid) throws Error;
+		public abstract async void notify_exec_completed (uint pid) throws Error;
 		public abstract async void wait_until_suspended (uint pid) throws Error;
 		public abstract async void cancel_pending_waits (uint pid) throws Error;
 		public abstract async void input (uint pid, uint8[] data) throws Error;
@@ -51,6 +52,7 @@ namespace Frida {
 		public abstract async uint spawn (string path, HostSpawnOptions options) throws GLib.Error;
 		public abstract async void launch (string identifier, HostSpawnOptions options) throws GLib.Error;
 		public abstract async void notify_launch_completed (string identifier, uint pid) throws GLib.Error;
+		public abstract async void notify_exec_completed (uint pid) throws GLib.Error;
 		public abstract async void wait_until_suspended (uint pid) throws GLib.Error;
 		public abstract async void cancel_pending_waits (uint pid) throws GLib.Error;
 		public abstract async void input (uint pid, uint8[] data) throws GLib.Error;